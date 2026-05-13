f39b6bd8ccf0d6d1aa35035cffc6d70ac6779ebe
Hasitha Adikari <adikari.adikari@mycit.ie>
2026-05-13 22:15:06 +0100

    add model counts as a comment

--- src/train_unified_wandb_updated_v1.py
+++ src/train_unified_wandb_updated_v1.py
@@ -1177,19 +1177,19 @@ def train_single_model(config: dict):
 def train_all_models(base_config: dict):
     print(f'train_all_models|base_config: {base_config}')
     all_models = {
-        # CNN baselines
+        # CNN baselines = 5
         'unet'                : [],
         'unetpp'              : [],
         'resunetpp'           : [],
         'deeplabv3plus'       : [],
         'deeplabv3plus_cbam'  : [],
-        # Transformers
+        # Transformers = 3
         'segformer'           : ['b0', 'b2'],
         'swin_unet'           : ['tiny'],
-        # Hybrid SOTA
+        # Hybrid SOTA = 6
         'convnext_upernet'    : ['tiny', 'small', 'base'],
         'hrnet_ocr'           : ['w18', 'w32', 'w48'],
-        # # Foundation models
+        # # Foundation models = 15
         'sam'                 : ['vit_b', 'vit_l', 'vit_h'],
         'sam_fpn'             : ['vit_b', 'vit_l', 'vit_h'],
         'dinov2'              : ['vit_s', 'vit_b', 'vit_l',],
