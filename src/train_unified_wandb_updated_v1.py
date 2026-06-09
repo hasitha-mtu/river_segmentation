df07d2bed24995c326ef2a57b337924f264b24ab
Hasitha Adikari <adikari.adikari@mycit.ie>
2026-06-09 08:52:49 +0100

    train all models, with  stratified  with seed 456

--- src/train_unified_wandb_updated_v1.py
+++ src/train_unified_wandb_updated_v1.py
@@ -1133,17 +1133,17 @@ def get_default_config():
         },
         'logging': {
             'use_wandb'       : True,
-            'wandb_project'   : 'river-segmentation-v2.8',
+            'wandb_project'   : 'river-segmentation-v2.9',
             'wandb_notes'     : 'UAV river segmentation — global-local dual-branch',
             'watch_model'     : False,
             'log_images'      : True,
             'save_model_wandb': True,
         },
         'system': {
-            'seed'         : 123,
+            'seed'         : 456,
             'num_workers'  : 0,
-            'output_dir'   : './experiments/',
-            # 'output_dir'   : r'D:/hasitha/river_segmentation/experiments/',
+            # 'output_dir'   : './experiments/',
+            'output_dir'   : r'D:/hasitha/river_segmentation/experiments/',
             'log_interval' : 10,
             'save_interval': 10,
         },
@@ -1271,8 +1271,8 @@ def train_all_models(base_config: dict):
 
 def main():
     # dataset_variations  = ['sequential', 'stratified', 'alternative']
-    dataset_variations  = ['stratified', 'alternative']
-    # dataset_variations  = ['stratified']
+    # dataset_variations  = ['stratified', 'alternative']
+    dataset_variations  = ['stratified']
     for dataset_variation in dataset_variations:
 
         default_config = get_default_config()
